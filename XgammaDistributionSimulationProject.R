e4821aa1d11ca4fbf18a81d5f95036e820dac744
Elom Warren <elom.kodjoh@gmail.com>
2024-03-30 23:26:35 +0000

    optimizing simulation code part1

--- XgammaDistributionSimulationProject.R
+++ XgammaDistributionSimulationProject.R
@@ -1,76 +1,37 @@
-
-
 # Simulation Studies for Xgamma distribution
 
-# library(optimx) # advanced version of optim
-# library(xtable)
 # library(tidyr) # presentation of data
 # library(dplyr) # data manipulation
-# library(MASS) # fitdistr
-pacman::p_load(tidyr, dplyr, MASS, optimx, xtable)
+# library(MASS) # for fitdistr
+pacman::p_load(tidyr, dplyr, MASS)
 
 
 
-## Generating Observations from the Xgamma distribution
+### Function to generate Xgamma observations
 
-# Secondary function to choose which value to pick
-
-# "CHOOSE" = function(u, x, y, thres)
-# {
-#   if (u <= thres)
-#   {
-#     return(x)
-#   }
-#   else
-#   {
-#     return(y)
-#   }
-# }
-
-
-
-### Function to generate random XGamma observations
-
-"rxgamma" = function(n, Theta)
+"rxgamma" = function(n, rate)
 {
+  threshold = rate / (1+rate)
   UNIF = runif(n = n, min = 0, max = 1)
-  EXP = rexp(n = n, rate = Theta)
-  GAMMA = rgamma(n = n, shape = 3, scale = 1/Theta)
-  
-    "CHOOSE" = function(u, x, y, thres)
-  {
-    if (u <= thres)
-    {
-      return(x)
-    }
-    else
-    {
-      return(y)
-    }
-  }
-  
-  Obs = mapply(CHOOSE, UNIF, EXP, GAMMA, thres = Theta/(1+Theta))
-  return(Obs)
+  EXP = rexp(n = n, rate = rate)
+  GAMMA = rgamma(n = n, shape = 3, rate = rate)
+  XGAMMA = ifelse(UNIF <= threshold, EXP, GAMMA) 
+  return(XGAMMA)
 }
 
+# Sample sizes and parameter values objects
 
+N = c(20, 40, 100) # Sample sizes
 
-
-# Setting up sample sizes and parameter values
-
-# Sample sizes
-n_values = c(20, 40, 100)
-# Parameter values
-Theta_values = c(0.1, 0.5, 1.0, 1.5, 3, 6)
+THETA = c(0.1, 0.5, 
+      1.0, 1.5, 3, 6) # Parameter values
 
 # Generate combinations of sample sizes and parameters
-size_param_combos = expand.grid(n = n_values, Theta = Theta_values)
+size_param_combos = expand.grid(n = N, rate = THETA)
 
 size_param_combos
 
-
-
-### Generate Xgamma observations for all combinations of sample size and parameter Theta
+### Generate Xgamma observations for all combinations of sample size and parameter rate
 
 
 list_Data = lapply(
@@ -78,202 +39,109 @@ list_Data = lapply(
   function(i) # Creating an anonymous function 
   {
     rxgamma(n = size_param_combos$n[i], 
-            Theta = size_param_combos$Theta[i])
+            rate = size_param_combos$rate[i])
   }
   )
 
-# Create names based on combinations of n and Theta
+# Create names based on combinations of n and rate
 combination_names = paste("n", size_param_combos$n, 
-                          "_Theta", size_param_combos$Theta, 
+                          "_rate", size_param_combos$rate, 
                           sep = "")
 
 # Set names for the list elements
-# setNames(list_Data, combination_names) # stopped working?!
-
 names(list_Data) = combination_names
 
 
-
-
-# HISTOGRAMS of Data
-
-# HIST = lapply(1:length(list_Data),
-#               function(i)
-#               {
-#                 hist(list_Data[[i]],
-#                      xlab = paste("Observations of Data",
-#                                   names(list_Data)[i]), main = "")
-#               }
-# )
-
-# Histogram for dataset with n=20 and Theta = 0.1
-# hist(list_Data[[1]], 
-#      xlab = paste("Observations of Data", 
-#                   names(list_Data)[1]), main = "")
-
-
-
-# DENSITY Graph of Data
-
-# PLOT DENSITY
-
-# DENSITY = lapply(
-#   1:length(list_Data),
-#   function(i)
-#   {
-#     plot(density(list_Data[[i]]),
-#          xlab = paste("Density" ,
-#                       names(list_Data)[i]), main = "")
-#   }
-#   )
-
-# Density for parameter combinations with size 20
-
-DENSITY_n20 = lapply(
-  c(1, 4, 7, 10, 13, 16),
-  function(i)
-  {
-     plot(density(list_Data[[i]]),
-          xlab = paste("Density" ,
-                       names(list_Data)[i]), main = "")
-  }
-)
-
-
-
-
-## Probability density function pdf of the Xgamma distribution
-
-
-### Function for pdf of Xgamma
-
-# x > 0, Theta > 0
-"den_xgamma" = function(Theta, x)
+# Density function of Xgamma
+# x > 0, rate > 0
+"dxgamma" = function(x, rate)
 {
-  (Theta^2 / (1 + Theta))*(1 + (Theta/2)*(x^2))*(exp(-(Theta*x)))
+  (rate^2 / (1 + rate))*(1 + (rate/2)*(x^2))*(exp(-(rate*x)))
 }
 
 
-
-# Graph pdf of Xgamma(Theta) for selected values of Theta
+# Plot density function of Xgamma for selected values of theta
 
 # Define the range of x values
-x_values = seq(0, 10, length.out = 1000)
+X = seq(0, 10, length.out = 500)
 
-# Specify Theta value
-Theta_values_pdf = c(0.5, 1, 5, 10) 
+# Specify rate value
+THETAS.fx = c(0.5, 1, 5, 10) 
 
-# Density values for all Thetas using den_xgamma function
-density_values = sapply(Theta_values_pdf, 
-                        function(Theta) den_xgamma(Theta, x_values))
+# Density values for all rates using dxgamma function
+Y = sapply(THETAS.fx, 
+           function(rate) dxgamma(X, rate=rate))
 
 # Plot the density functions
-matplot(x_values, density_values, 
-        type = "l", col = rainbow(length(Theta_values_pdf)), 
+matplot(X, Y, 
+        type = "l", col = rainbow(length(THETAS.fx)), 
         lty = 1:4, lwd = 2,
         xlab = "x", ylab = "f(x)", ylim = c(0, 0.5), 
         main = "Xgamma Density Functions")
 
 # Add a legend
 legend("topright", 
-       legend = paste("Theta =", Theta_values_pdf), 
-       col = rainbow(length(Theta_values_pdf)), 
+       legend = paste("theta =", THETAS.fx), 
+       col = rainbow(length(THETAS.fx)), 
        lty = 1:4, lwd = 2, bty = "n")
-
-
-
 
 
 ## Log-Likelihood Function for Xgamma
 
-
-"loglik_Xgamma" = function(Theta, x)
+"loglik.xgamma" = function(rate, x)
 {
   n = length(x)
-  loglik = 2*n*log(Theta) - n*log(1 + Theta) + 
-    sum(log(1 + (Theta/2)*(x^2))) - Theta*sum(x)
+  loglik = 2*n*log(rate) - n*log(1 + rate) + 
+    sum(log(1 + (rate/2)*(x^2))) - rate*sum(x)
   
   return(-loglik) 
 }
 
 
-
-## Finding Maximum Likelihood Estimator MLE Theta.Hat of Theta
-
-
-# vec_Theta.Hat = lapply(
-#   1:length(list_Data),
-#   function(i)
-#   {
-#     tryCatch(unlist(
-#          optimx(par = 0.2,
-#            fn = loglik_Xgamma,
-#            method = "nlminb",
-#            x = list_Data[[i]]
-#     )),
-#     error = function(e)rep(NA))
-#   }
-# )
-
-
-# Try and Error on optim function to find MLE
-
-vec_Theta.Hat = sapply(
-  1:length(list_Data),
-  function(i)
-  {
-    optim(par = 0.2,
-          fn = loglik_Xgamma, 
-          x = list_Data[[i]], 
-          method = c("Brent"), 
-          lower = 0, upper = 3000,
-          hessian = T
-          )
-  }
-)
-
-
-
-# vec_Theta.Hat
-# # Parameter value
-# vec_Theta.Hat[[1]]
-
-
+## Function to compute Maximum Likelihood Estimate MLE theta.hat of theta
+"theta.ML" = function(x)
+{
+  theta.optim = optim(par=0.1, 
+                      fn = loglik.xgamma,
+                      method = "Brent", 
+                      lower = 0, upper = 5000,
+                      hessian = FALSE)
+  return(theta.optim$par)
+}
 
 ### MSE and Bias function (Secondary function)
 
-"Bias.MSE" = function(Theta.hat, Theta)
+"Bias.MSE" = function(est, param)
 {
-  Bias = mean(Theta.hat - Theta)
-  MSE = mean((Theta.hat - Theta)^2)
+  # est = vector of estimates
+  # param = parameter to estimate
+  Bias = mean(est - param)
+  MSE = mean((est - param)^2)
   tab = round(c(Bias, MSE), 5)
   names(tab) = c("Bias", "MSE")
-  # tab = round(data.frame(Bias = Bias, MSE = MSE), 5)
   return(tab)
 }
 
 
 ## Monte Carlo Simulation function for Xgamma
-# Generates Average Bias and MSE of the estimator Theta.hat
+# Generates Bias and MSE of the estimator theta.hat
 
-
-
-"simple.Sim.xgamma" = function(N, n, Theta)
+"simple.Sim.xgamma" = function(N, n, rate)
 {
-  # Generate Xgamma observations
-  Xgamma_data = sapply(
+  # Generate samples from Xgamma distribution 
+  samples = sapply(
     1:N,
-    function(i) rxgamma(n=n, Theta = Theta)
+    function(i) rxgamma(n=n, rate = rate)
   )
   
   # Maximum Likelihood 
-  res.Theta.hat = apply(
-    Xgamma_data, 
+  res.rate.hat = apply(
+    samples, 
     MARGIN = 2, # function applied to columns
     function(x)
     {
       optim(par = 0.2,
-            fn = loglik_Xgamma, 
+            fn = loglik.xgamma, 
             x = x, 
             method = c("Brent"), 
             lower = 0, upper = 3000,
@@ -282,14 +150,14 @@ vec_Theta.Hat = sapply(
     }
   )
   
-  # Finding Theta.hat
-  Theta.hat = sapply(
+  # Finding rate.hat
+  rate.hat = sapply(
     1:N,
-    function(x)res.Theta.hat[[x]]$par
+    function(x)res.rate.hat[[x]]$par
   )
   
   # Bias and MSE computation
-  tab = Bias.MSE(Theta.hat = Theta.hat, Theta = Theta)
+  tab = Bias.MSE(rate.hat = rate.hat, rate = rate)
 
   return(tab)
 }
@@ -299,8 +167,8 @@ vec_Theta.Hat = sapply(
 
 N = 10000
 n = 20
-Theta = 0.1
-simple.Sim.xgamma(N=N, n=n, Theta = Theta)
+rate = 0.1
+simple.Sim.xgamma(N=N, n=n, rate = rate)
 
 
 
@@ -309,27 +177,27 @@ simple.Sim.xgamma(N=N, n=n, Theta = Theta)
 N = 10000
 n = 20
 Bias_MSE_mat = data.frame(
-  t(sapply(Theta_values, 
-           function(Theta)
-             simple.Sim.xgamma(N=N, n=n, Theta = Theta))))
+  t(sapply(THETA, 
+           function(rate)
+             simple.Sim.xgamma(N=N, n=n, rate = rate))))
 
 
 
-### Calling simulation function for selected values of n and Theta
+### Calling simulation function for selected values of n and rate
 
-n.Theta = expand.grid(Theta = Theta_values, n = n_values)
-N = rep(10000, length(n.Theta))
+n.rate = expand.grid(rate = THETA, n = N)
+N = rep(10000, length(n.rate))
 
 
 
-### Simulation function for all values of n and Theta
+### Simulation function for all values of n and rate
 
-"Sim.xgamma" = function(N, n, Theta)
+"Sim.xgamma" = function(N, n, rate)
 {
   Bias.MSE_matrix = data.frame(
     t(
-      mapply(simple.Sim.xgamma, N=N, n=n, Theta = Theta)), 
-    row.names = paste("n", n, "_Theta", Theta, sep = ""))
+      mapply(simple.Sim.xgamma, N=N, n=n, rate = rate)), 
+    row.names = paste("n", n, "_rate", rate, sep = ""))
 
   return(Bias.MSE_matrix)
 }
@@ -339,22 +207,22 @@ N = rep(10000, length(n.Theta))
 
 # Simulation function for exponential distribution
 
-"simple.Sim.exp" = function(N, n, Theta)
+"simple.Sim.exp" = function(N, n, rate)
 {
   # Generate random data from exponential distribution
-  exp.Data = rexp(n = n, rate = Theta)
+  exp.Data = rexp(n = n, rate = rate)
   
   # Maximum Likelihood estimation
-  res.Theta.hat = fitdistr(
+  res.rate.hat = fitdistr(
     x = exp.Data, 
     densfun = "exponential"
   )
   
   # Return MLE
-  Theta.hat = res.Theta.hat$estimate
+  rate.hat = res.rate.hat$estimate
   
   # Compute Bias and MSE
-  tab = Bias.MSE(Theta.hat = Theta.hat, Theta = Theta)
+  tab = Bias.MSE(rate.hat = rate.hat, rate = rate)
   
   return(tab)
 }
@@ -364,18 +232,18 @@ N = rep(10000, length(n.Theta))
 
 N = 10000
 n = 20
-Theta = 0.1
-simple.Sim.exp(N=N, n=n, Theta = Theta)
+rate = 0.1
+simple.Sim.exp(N=N, n=n, rate = rate)
 
 
 ### Main simulation function for exponential
 
-"Sim.exp" = function(N, n, Theta)
+"Sim.exp" = function(N, n, rate)
 {
   Bias.MSE_matrix = data.frame(
     t(
-      mapply(simple.Sim.exp, N=N, n=n, Theta = Theta)), 
-    row.names = paste("n", n, "_Theta", Theta, sep = ""))
+      mapply(simple.Sim.exp, N=N, n=n, rate = rate)), 
+    row.names = paste("n", n, "_rate", rate, sep = ""))
 
   return(Bias.MSE_matrix)
 }
@@ -386,11 +254,11 @@ simple.Sim.exp(N=N, n=n, Theta = Theta)
 
 # For Xgamma
 
-Bias.MSE.xgamma = Sim.xgamma(N, n.Theta$n, n.Theta$Theta)
+Bias.MSE.xgamma = Sim.xgamma(N, n.rate$n, n.rate$rate)
 
 Bias.MSE.xgamma.df = data.frame(
-  n = rep(n_values, each = 6),
-  Theta = rep(Theta_values, times = 3),
+  n = rep(N, each = 6),
+  rate = rep(THETA, times = 3),
   Bias = Bias.MSE.xgamma$Bias,
   MSE = Bias.MSE.xgamma$MSE
 )
@@ -405,11 +273,11 @@ Sim.Bias.MSE.xgamma = Bias.MSE.xgamma.df %>%
 
 # For Exponential
 
-Bias.MSE.exp = Sim.exp(N, n.Theta$n, n.Theta$Theta)
+Bias.MSE.exp = Sim.exp(N, n.rate$n, n.rate$rate)
 
 Bias.MSE.exp.df = data.frame(
-  n = rep(n_values, each = 6),
-  Theta = rep(Theta_values, times = 3),
+  n = rep(N, each = 6),
+  rate = rep(THETA, times = 3),
   Bias = Bias.MSE.exp$Bias,
   MSE = Bias.MSE.exp$MSE
 )
